4b31fd2702a0ca645dff91378299b57bbe9bab7d
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-14 23:00:25 +0200

    src/index.js updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import _ from 'lodash';
+import { HEADER } from "./js/header";
+import { HOME_PAGE } from './js/home';
 
 function component() {
-  const element = document.createElement('div');
+  const PAGE = document.querySelector('#content')
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  PAGE.innerHTML = HEADER;
+  PAGE.innerHTML += HOME_PAGE;
 
-  return element;
+  return PAGE;
 }
 
 document.body.appendChild(component());
