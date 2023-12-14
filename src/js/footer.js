c78c40f478bf43236701185101c156453b788ef6
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-14 23:25:01 +0200

    src/js/footer.js added in project

---
+++ src/js/footer.js
@@ -0,0 +1,10 @@
+// FOOTER TO RESTAURANT PAGE
+
+const FOOTER = `
+  <div class="footer>
+    <p>This site, and the pizza, were developed by &copy;
+    <a url="https://www.linkedin.com/in/rostyslav-putnikov-76484926a/"> Rostislav-Coder</a> using the latest achievements of space science.</p>
+  </div>
+`;
+
+export { FOOTER };
