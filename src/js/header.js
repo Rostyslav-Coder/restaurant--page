ec8794539ac116122bffe6d0cce65fbe5cf1a458
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-14 23:06:08 +0200

    src/js/header.js added in project

---
+++ src/js/header.js
@@ -0,0 +1,25 @@
+// HEADER TO RESTAURANT PAGE
+
+const HEADER = `
+  <div class="header" id="header" >
+    <div class="logo"  id="logo">
+      <div>m</div>
+      <img src="../dist/images/moon.png" alt="logotip" />
+      <div>onPizza</div>
+    </div>
+      <div class="button" id="home">
+      <p>Home</p>
+      </div>
+    <div class="button" id="menu">
+      <p>Menu</p>
+    </div>
+    <div class="button" id="about">
+      <p>About</p>
+    </div>
+    <div class="button" id="contact">
+      <p>Contact</p>
+    </div>
+  </div>
+`;
+
+export { HEADER };
