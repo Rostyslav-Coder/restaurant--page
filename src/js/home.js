d9a69870582bf44fedc05af64d1351d6347e06fe
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-14 23:06:39 +0200

    src/js/home.js added in project

---
+++ src/js/home.js
@@ -0,0 +1,27 @@
+// HOME PAGE TO RESTAURANT PAGE
+
+const HOME_PAGE = `
+  <div class="page">
+    <div class="bg-lower"></div>
+    <div class="text parallax">
+      <p>There is only one <span>real Pizzeria</span> on the Moon</p>
+    </div>
+    <div class="earth parallax">
+      <img src="../dist/images/earth.png" alt="earth" />
+    </div>
+    <div class="bg-top parallax">
+      <img src="../dist/images/moon-landscape.png" alt="moon landscape" />
+    </div>
+    <div class="moon-shatle parallax">
+      <img src="../dist/images/moon-station.png" alt="moon shatle" />
+    </div>
+    <div class="pizza-man-1 parallax">
+      <img src="../dist/images/pizza-man-1.png" alt="pizza man" />
+    </div>
+    <div class="pizza-man-2 parallax">
+      <img src="../dist/images/pizza-man-3.png" alt="pizza man" />
+    </div>
+  </div>
+`;
+
+export { HOME_PAGE };
