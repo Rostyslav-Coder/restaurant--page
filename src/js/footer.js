dfdb01b67d6ebcf1d8fbe96aaa059e6306df9e86
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-15 23:33:30 +0200

    src/js/footer.js updated

--- src/js/footer.js
+++ src/js/footer.js
@@ -2,8 +2,8 @@
 
 const FOOTER = `
   <div class="footer">
-    <p>This site, and the pizza, were developed by &copy;
-    <a href="https://www.linkedin.com/in/rostyslav-putnikov-76484926a/"> Rostislav-Coder</a> using the latest achievements of space science.</p>
+    <p>This site, and the pizza, were developed by 
+    <a href="https://www.linkedin.com/in/rostyslav-putnikov-76484926a/">&copy; Rostislav-Coder</a> using the latest achievements of space science.</p>
   </div>
 `;
 
