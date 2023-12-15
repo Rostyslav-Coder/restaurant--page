78562f7648ec931affbf82b9d2b8e10bd4da20b6
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-15 16:34:33 +0200

    src/js/home.js updated

--- src/js/home.js
+++ src/js/home.js
@@ -1,24 +1,26 @@
 // HOME PAGE TO RESTAURANT PAGE
 
 const HOME_PAGE = `
-  <div class="page">
-    <div class="bg-lower"></div>
+  <div class="page"> 
+    <div class="back-bg">
+      <img src="../dist/images/universe.png" alt="earth" />
+    </div>
     <div class="text parallax">
       <p>There is only one <span>real Pizzeria</span> on the Moon</p>
     </div>
     <div class="earth parallax">
       <img src="../dist/images/earth.png" alt="earth" />
     </div>
-    <div class="bg-top parallax">
+    <div class="front-bg parallax">
       <img src="../dist/images/moon-landscape.png" alt="moon landscape" />
     </div>
     <div class="moon-shatle parallax">
       <img src="../dist/images/moon-station.png" alt="moon shatle" />
     </div>
-    <div class="pizza-man-1 parallax">
+    <div class="pizza-man-1 astronaut">
       <img src="../dist/images/pizza-man-1.png" alt="pizza man" />
     </div>
-    <div class="pizza-man-2 parallax">
+    <div class="pizza-man-2 astronaut">
       <img src="../dist/images/pizza-man-3.png" alt="pizza man" />
     </div>
   </div>
