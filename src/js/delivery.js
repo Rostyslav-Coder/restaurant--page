d4bbeacb9a9a5e3b9c93f1fd1e2cb225d3907ed6
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-16 22:56:23 +0200

    src/js/delivery.js added in project

---
+++ src/js/delivery.js
@@ -0,0 +1,10 @@
+// DELIVERY PAGE TO RESTAURANT PAGE
+
+const DELIVERY_PAGE = `
+  <div class="about-page"> 
+    <h1>DElivery Page</h1>
+    <p>In progres</p>
+  </div>
+`;
+
+export { DELIVERY_PAGE };
