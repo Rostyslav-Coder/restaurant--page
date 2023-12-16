75da56020f7e2a4e13dee8566e40770a95e047d5
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-16 22:54:38 +0200

    src/index.js updated

--- src/index.js
+++ src/index.js
@@ -1,35 +1,12 @@
-// import _ from 'lodash';
-// import { HEADER } from "./js/header";
-// import { HOME_PAGE } from './js/home';
-// import { MENU_PAGE } from './js/menu';
-// import { FOOTER } from './js/footer';
-
-// function component(_page = HOME_PAGE) {
-//   const PAGE = document.querySelector('#content')
-
-//   // Lodash, now imported by this script
-//   PAGE.innerHTML = HEADER;
-//   PAGE.innerHTML += _page;
-//   PAGE.innerHTML += FOOTER;
-
-//   return PAGE;
-// }
-
-// document.body.appendChild(component());
-
-// const home = document.querySelector('#home');
-// const menu = document.querySelector('#menu');
-// const about = document.querySelector('#about');
-// const contact = document.querySelector('#contact');
-
-// const buttons = [home, menu, about, contact];
+// INDEX.JS
 
 import _ from 'lodash';
 import { HEADER } from "./js/header";
 import { HOME_PAGE } from './js/home';
 import { MENU_PAGE } from './js/menu';
-import { ABOUT_PAGE } from './js/about';
+import { DELIVERY_PAGE } from './js/delivery';
 import { CONTACT_PAGE } from './js/contact';
+import { ABOUT_PAGE } from './js/about';
 import { FOOTER } from './js/footer';
 
 function component(_page = HOME_PAGE) {
@@ -45,11 +22,12 @@ function component(_page = HOME_PAGE) {
 
   const home = document.querySelector('#home');
   const menu = document.querySelector('#menu');
-  const about = document.querySelector('#about');
+  const delivery = document.querySelector('#delivery');
   const contact = document.querySelector('#contact');
+  const about = document.querySelector('#about');
 
-  const buttons = [home, menu, about, contact];
-  const pages = [HOME_PAGE, MENU_PAGE, ABOUT_PAGE, CONTACT_PAGE];
+  const buttons = [home, menu, delivery, contact, about];
+  const pages = [HOME_PAGE, MENU_PAGE, DELIVERY_PAGE, CONTACT_PAGE, ABOUT_PAGE];
 
   const buttonPageMap = {};
   buttons.forEach((button, index) => {
