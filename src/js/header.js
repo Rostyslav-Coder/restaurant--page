08768dd4137b7b681e127ee30732cb224a84c689
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-16 22:55:30 +0200

    src/js/header.js updated

--- src/js/header.js
+++ src/js/header.js
@@ -13,11 +13,14 @@ const HEADER = `
     <div class="button" id="menu">
       <p>Menu</p>
     </div>
-    <div class="button" id="about">
-      <p>About</p>
+    <div class="button" id="delivery">
+      <p>Delivery</p>
     </div>
     <div class="button" id="contact">
       <p>Contact</p>
+    </div>
+    <div class="button" id="about">
+      <p>About</p>
     </div>
   </div>
 `;
