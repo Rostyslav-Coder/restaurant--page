f3680330efa69a314010119475e1ea6f13c39a22
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-16 17:21:37 +0200

    src/js/menu.js added in project

---
+++ src/js/menu.js
@@ -0,0 +1,169 @@
+// MENU PAGE TO RESTAURANT PAGE
+
+const MENU_PAGE = `
+  <div class="menu-page">
+
+    <div class="dish">
+      <h1>Cosmo-Cowboy Pizza</h1>
+      <div>
+        <img src="../dist/images/beef-pizza.png" />
+        <p>
+          This pizza is for those who love some real beef and ain’t afraid of no alien abductions.
+          Veal, barbecue sauce, onion and cheddar cheese - this combo will keep you from getting bored.
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="" />
+    </div>
+
+    <div class="dish">
+      <h1>Crater Cheese Pizza</h1>
+      <div>
+        <img src="../dist/images/cheese-pizza.png" />
+        <p>
+          Our cheese is made from lunar cheese, which we mine from craters.
+          It has a unique taste and aroma that will blow your mind.
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="" />
+    </div>
+
+    <div class="dish">
+      <h1>Radioactive Pizza</h1>
+      <div>
+        <img src="../dist/images/mushroom-pizza.png" />
+        <p>
+          Our mushrooms are collected on the dark side of the Moon, where they grow under the influence of cosmic radiation.
+          They have an unusual shape and color, and they glow in the dark. This pizza is for real explorers and adventure lovers.
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="" />
+    </div>
+
+    <div class="dish">
+      <h1>Exotic Pizza</h1>
+      <div>
+        <img src="../dist/images/exotic-pizza.png" />
+        <p>
+          Our pineapples are brought from a secret alien base, which is located on the far side of the Moon.
+          They have an incredibly sweet and fragrant taste, which will please any sweet tooth.
+          This pizza is for those who want to try something exotic and unusual.
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="" />
+    </div>
+
+    <div class="dish">
+      <h1>Snorting Astronaut</h1>
+      <div>
+        <img src="../dist/images/pork-burger.png" />
+        <p>
+          Our bacon is the meat of lunar pigs, who live in special domes and feed on lunar herbs.
+          They are very juicy and tender, and their skin crunches on your teeth.
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="" />
+    </div>
+
+    <div class="dish">
+      <h1>Cosmic Chicken</h1>
+      <div>
+        <img src="../dist/images/chicken-burger.png" />
+        <p>
+          Our chicken is the meat of lunar chickens, who hatch from eggs that we find in meteorites.
+          They have tender and juicy meat, which is fried to a crispy crust.
+          This burger is for those who love classic with an unexpected accent.
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="" />
+    </div>
+
+    <div class="dish">
+      <h1>Galactic Fish</h1>
+      <div>
+        <img src="../dist/images/fish-burger.png" />
+        <p>
+          Our fish is the meat of lunar fish, who live in underground lakes, which we open by drilling.
+          They have a transparent body and glowing eyes, and a unique taste and smell.
+          This burger is for those who want to try something fresh and original.
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="" />
+    </div>
+
+    <div class="dish">
+      <h1>Starry Potato</h1>
+      <div>
+        <img src="../dist/images/french-potato.png" />
+        <p>
+          Our potatoes are grown on lunar soil, which is enriched with minerals and vitamins.
+          They have a golden color and a crispy crust, and inside they are soft and fluffy.
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="" />
+    </div>
+
+    <div class="dish">
+      <h1>Lunar Water</h1>
+      <div>
+        <img src="../dist/images/water.png" />
+        <p>
+          Our water is the water that flows from lunar geysers, which we activate with special devices.
+          It has a clean and pleasant taste, and contains many beneficial trace elements.
+          This drink is for those who want to quench their thirst and support their health.
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="" />
+    </div>
+
+    <div class="dish">
+      <h1>Fruit Juice</h1>
+      <div>
+        <img src="../dist/images/juice.png" />
+        <p>
+          Our juice is the juice that we make from lunar fruits, which we grow in greenhouses.
+          It has a bright and saturated color, and a sweet and sour taste.
+          This drink is for those who want to get a boost of energy and vitamins.
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="" />
+    </div>
+
+    <div class="dish">
+      <h1>Beer Rocket</h1>
+      <div>
+        <img src="../dist/images/beer.png" />
+        <p>
+          Our beer is the beer that we brew from lunar malt, which we get from lunar dust.
+          It has a foamy and bubbly texture, and a bitter and refreshing taste.
+          This drink is for those who want to relax and enjoy the atmosphere.
+        </p>
+      </div>
+    </div>
+    <div class="image">
+      <img src="" />
+    </div>
+
+  </div>
+`;
+
+export { MENU_PAGE };
