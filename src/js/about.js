c37fc9ec0f2329fc206298d6350cd7bebf32887d
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-17 01:16:41 +0200

    src/js/about.js updated

--- src/js/about.js
+++ src/js/about.js
@@ -1,11 +1,16 @@
 // ABOUT PAGE TO RESTAURANT PAGE
 
 const ABOUT_PAGE = `
-  <div class="about-page"> 
-    <h1>About</h1>
-    <p>
-      We, the members of the expedition to the Moon, decided to stay here and open a pizzeria. Why? Because we found something amazing on the Moon, that changed our view of life. We found an ancient artifact, that showed us that we are not alone in the universe. It also gave us a recipe for pizza, that was the most delicious in the world. We decided that this was a sign of fate, and that we had to share this pizza with others. We remodeled our lunar module into a pizzeria, and started to make pizza according to the recipe of the artifact. We named our pizzeria "Lunar Pizza", and became famous in the whole cosmos. We are happy and satisfied with our life, and we don't regret anything. And if you hear something about the gravity acceleration around the Moon, don't believe it. It's just that all the astronauts come to us for pizza.
-    </p>
+  <div class="about-page">
+    <div class="card">
+      <h1>About</h1>
+      <div class="sub-card">
+        <p>
+          We, the members of the expedition to the Moon, decided to stay here and open a pizzeria. Why? Because we found something amazing on the Moon, that changed our view of life. We found an ancient artifact, that showed us that we are not alone in the universe. It also gave us a recipe for pizza, that was the most delicious in the world. We decided that this was a sign of fate, and that we had to share this pizza with others. We remodeled our lunar module into a pizzeria, and started to make pizza according to the recipe of the artifact. We named our pizzeria "Lunar Pizza", and became famous in the whole cosmos. We are happy and satisfied with our life, and we don't regret anything. And if you hear something about the gravity acceleration around the Moon, don't believe it. It's just that all the astronauts come to us for pizza.
+        </p>
+        <img src="../dist/images/pizza-team.png" />
+      </div>
+    </div>
   </div>
 `;
 
