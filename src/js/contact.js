0dc8ffce0ed6b99a207babb5c9d570416067776b
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-16 17:22:42 +0200

    src/js/contact.js added in project

---
+++ src/js/contact.js
@@ -0,0 +1,10 @@
+// CONTACT PAGE TO RESTAURANT PAGE
+
+const CONTACT_PAGE = `
+  <div class="contact-page">
+      <h1>Contact Page</h1>
+      <p>In Progres</p>
+  </div>
+`;
+
+export { CONTACT_PAGE };
