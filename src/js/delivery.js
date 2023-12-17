39f5c084d09ecaf5025904f4b5f3a3f544969a4e
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-17 16:19:05 +0200

    src/js/delivery.js updated

--- src/js/delivery.js
+++ src/js/delivery.js
@@ -1,9 +1,28 @@
 // DELIVERY PAGE TO RESTAURANT PAGE
 
 const DELIVERY_PAGE = `
-  <div class="about-page"> 
-    <h1>DElivery Page</h1>
-    <p>In progres</p>
+  <div class="delivery-page"> 
+    <div class="back-bg">
+      <img src="../dist/images/universe.png" alt="universe" />
+    </div>
+    <div class="delivery parallax">
+      <p>We can send you a courier rocket for <span>$1,000,000,000</span>.</p>
+      <img src="../dist/images/delivery.png" alt="delivery roket" />
+    </div>
+    <div class="earth parallax">
+      <img src="../dist/images/earth.png" alt="earth" />
+    </div>
+    <div class="front-bg parallax">
+      <img src="../dist/images/moon-landscape.png" alt="moon landscape" />
+    </div>
+    <div class="free-delivery parallax">
+      <P>We can catapult your order onto your head <br><span>for free</span> as we fly overhead.</p>
+      <img src="../dist/images/free-delivery.png" alt="free-delivery mashine" />
+    </div>
+    <div class="delivery-man astronaut">
+      <p>In any case, your order will be delivered to you within <span>one hour</span>.</p>
+      <img src="../dist/images/delivery-man.png" alt="delivery man" />
+    </div>
   </div>
 `;
 
