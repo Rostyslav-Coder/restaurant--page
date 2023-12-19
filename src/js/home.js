492b22931603231028f584d93bff4fb2cd265ec3
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-19 23:28:56 +0200

    src/js/home.js updated

--- src/js/home.js
+++ src/js/home.js
@@ -11,23 +11,23 @@ const HOME_PAGE = `
     <div class="earth parallax">
       <img src="../dist/images/earth.png" alt="earth" />
     </div>
-    <div class="front-bg parallax">
+    <div class="front-bg">
       <img src="../dist/images/moon-landscape.png" alt="moon landscape" />
     </div>
-    <div class="moon-shatle parallax">
-      <P>I only use the freshest ingredients.</p>
-      <p>Just don’t ask where I get them from.</p>
+    <div class="moon-shatle">
       <img src="../dist/images/moon-station.png" alt="moon shatle" />
+      <p>I only use the freshest ingredients.</p>
+      <p>Just don’t ask where I get them from.</p>
     </div>
     <div class="pizza-man-1 astronaut">
+      <img src="../dist/images/pizza-man-1.png" alt="pizza man" />
       <p>We make the most delicious pizza on the entire moon.</p>
       <p>This is the only pizzeria on the moon.</p>
-      <img src="../dist/images/pizza-man-1.png" alt="pizza man" />
     </div>
     <div class="pizza-man-2 astronaut">
+      <img src="../dist/images/pizza-man-3.png" alt="pizza man" />
       <p>We can deliver pizza to you anywhere in the world.</p>
       <p>We'll drop it right on your doorstep when we're passing by.</p>
-      <img src="../dist/images/pizza-man-3.png" alt="pizza man" />
     </div>
   </div>
 `;
