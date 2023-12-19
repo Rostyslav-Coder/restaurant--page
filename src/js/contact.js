b7eda4c0042175faf6425ef192a6c3486dfc6f64
Rostyslav <rostyslavputnikov@gmail.com>
2023-12-19 16:25:30 +0200

    src/js/contact.js updated

--- src/js/contact.js
+++ src/js/contact.js
@@ -2,8 +2,23 @@
 
 const CONTACT_PAGE = `
   <div class="contact-page">
-      <h1>Contact Page</h1>
-      <p>In Progres</p>
+    <div class="back-bg">
+    </div>
+    <div class="text">
+      <p>
+        "MoonPizza" is located in the southwestern region of the "Sea of Tranquility" on the Moon. This place is located almost on the equator of the Moon. From orbit you can clearly see the landing site of our "MoonPizza" module and its long shadow. We installed a US flag nearby and placed a set of auxiliary structures. We look forward to your visit.
+      </p>
+    </div>
+    <div class="pizza-man-3">
+      <img src="../dist/images/pizza-man-1.png" alt="pizza-man" />
+    </div>
+    <div class="pizza-man-4">
+      <img src="../dist/images/pizza-man-3.png" alt="pizza-man" />
+    </div>
+    <div class="delivery-man-2">
+      <img src="../dist/images/delivery-man.png" alt="delivery-man" />
+    </div>
+    <div id="mask"></div>
   </div>
 `;
 
